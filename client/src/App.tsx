1b2443f4f6cbee807fef98bdc43f89ae2dda9ec7
Aditya Kumar Saini <adityasainics10@gmail.com>
2024-07-16 18:26:50 +0530

    initialized the frontend with tailwind and shadcn

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "./components/ui/button"
+
+const App = () => {
+  return (
+    <div className="text-rose-500">
+      App
+      <Button variant='destructive'>
+        click me 
+      </Button>
+    </div>
+  )
+}
+
+export default App
